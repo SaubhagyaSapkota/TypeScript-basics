1782d32da5de53b7f7ccf96603e3a425a35fb151
SaubhagyaSapkota <saubhagyasapkota444@gmail.com>
2025-07-01 12:35:35 +0545

    Utility: Omit used

--- src/utility.ts
+++ src/utility.ts
@@ -1,14 +1,32 @@
+// Use Pick to create PublicUser type with only name and email.
 type user = {
   id: number;
   name: string;
   email: string;
 };
 
-type Userprofile = Pick<user, "name" | "email">;
+type PublicUser = Pick<user, "name" | "email">;
 
-const res: Userprofile = {
+const res: PublicUser = {
   name: "saubhagya sapkota",
   email: "sau@gmail.com",
 };
 
 console.log(res);
+
+// Use Omit to create a type without password
+
+type Password = {
+  id: number;
+  name: string;
+  password: string;
+};
+
+type withoutPassword = Omit<Password, "password">;
+
+const res2: withoutPassword = {
+  id: 12,
+  name: "saubhagya",
+  //   password: "okokok",
+};
+console.log(res2);
