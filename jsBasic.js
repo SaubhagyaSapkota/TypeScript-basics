33896f3bc1408388d72eaebd2ee0040ef26ffb3e
SaubhagyaSapkota <saubhagyasapkota444@gmail.com>
2025-07-02 17:43:07 +0545

    scope shadowing

--- jsBasic.js
+++ jsBasic.js
@@ -13,13 +13,13 @@ function outer() {
 outer();
 
 //-------------------------------------------
-let x = 1;
+let x1 = 1;
 
 function foo() {
-  let x = 2;
+  let x1 = 2;
 
   function bar() {
-    console.log(x); // 2 (from nearest scope)
+    console.log(x1); // 2 (from nearest scope)
   }
 
   bar();
@@ -29,17 +29,48 @@ foo();
 
 //--------------------------------------
 
-let y = 1;
+let y1 = 1;
 
 function outer1() {
   function inner1() {
-    y = 5; // Modifies global `x`
+    y1 = 5; // Modifies global `x`
   }
 
   inner1();
 }
 
 outer1();
-console.log(y); // 5 ❗
+console.log(y1); // 5 ❗
 
 console.log("hi i have autosaved and done");
+
+// Let and Const
+
+const user = { name: "saub" };
+
+user.name = "sapko";
+console.log(user)
+
+
+//----------------------------------------------
+
+//Scope Shadowding
+let x = 10;
+
+{
+  let x = 20; // This shadows the outer x
+  console.log(x); // ✅ 20 (inner x)
+}
+
+console.log(x); // ✅ 10 (outer x)
+
+// //--------------------------------
+
+var y = 10;
+
+{
+  var y = 20; // This shadows the outer x
+  console.log(y); // ✅ 20 (inner x)
+}
+
+console.log(y); // ✅ 20 (outer x)
